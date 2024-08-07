023f83fd1bcfede60a75b85584516823fa5e9190
f.zarqani <zarqani.f94@gmail.com>
2024-08-07 20:11:39 +0300

    Fix other prettier problems

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,23 +2,23 @@
  * https://github.com/vercel/next.js/blob/canary/lint-staged.config.js
  */
 
-const { quote } = require("shell-quote");
+const { quote } = require('shell-quote');
 
-const isWin = process.platform === "win32";
+const isWin = process.platform === 'win32';
 
 module.exports = {
-  "**/*.{js,jsx,cjs,mjs,ts,tsx,mts,cts,md,json}": (filenames) => {
-    const escapedFileNames = filenames
-      .map((filename) => `"${isWin ? filename : escape([filename])}"`)
-      .join(" ");
-    return [
-      `prettier --write ${escapedFileNames}`,
-      `git add ${escapedFileNames}`,
-    ];
-  },
+	'**/*.{js,jsx,cjs,mjs,ts,tsx,mts,cts,md,json}': (filenames) => {
+		const escapedFileNames = filenames
+			.map((filename) => `"${isWin ? filename : escape([filename])}"`)
+			.join(' ');
+		return [
+			`prettier --write ${escapedFileNames}`,
+			`git add ${escapedFileNames}`,
+		];
+	},
 };
 
 function escape(str) {
-  const escaped = quote(str);
-  return escaped.replace(/\\@/g, "@");
+	const escaped = quote(str);
+	return escaped.replace(/\\@/g, '@');
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-        popup: resolve(__dirname, "popup.html"),
-        options: resolve(__dirname, "options.html"),
-        service_worker: resolve(__dirname, "src/background.ts"),
-        content_script: resolve(__dirname, "src/content-script.ts"),
-      },
-      output: {
-        chunkFileNames: "[name].[hash].js",
-        assetFileNames: "[name].[hash].[ext]",
-        entryFileNames: "[name].js",
-        dir: "dist",
-      },
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, 'index.html'),
+				popup: resolve(__dirname, 'popup.html'),
+				options: resolve(__dirname, 'options.html'),
+				service_worker: resolve(__dirname, 'src/background.ts'),
+				content_script: resolve(__dirname, 'src/content-script.ts'),
+			},
+			output: {
+				chunkFileNames: '[name].[hash].js',
+				assetFileNames: '[name].[hash].[ext]',
+				entryFileNames: '[name].js',
+				dir: 'dist',
+			},
+		},
+	},
 });
